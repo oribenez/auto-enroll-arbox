9accfab753a2b1c937c15338639ac7bbffde76bb
Ori Ben Ezra <oriben467@gmail.com>
2023-05-05 22:17:24 +0300

    emoji

--- app.js
+++ app.js
@@ -30,8 +30,7 @@ app.use((req, res, next) => {
 await scheduler();
 
 app.get("/", async (req, res, next) => {
-	console.log("Health check");
-	await loginArbox(); //test
+	console.log("Health check 🩸🧬");
 	
 	const healthcheck = {
 		uptime: process.uptime(),

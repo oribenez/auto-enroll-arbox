d33dd40d53da3b9cf68ba6cfbcf1dd1459e90775
Ori Ben Ezra <oriben467@gmail.com>
2023-05-05 17:25:57 +0300

    d

---
+++ data/config.js
@@ -0,0 +1,17 @@
+export default {
+  user_creds: {
+    email: process.env.ARBOX_USER_EMAIL,
+    password: process.env.ARBOX_USER_PASSWORD
+  },
+  registerTime: "16:46:00",
+  timezone: "Asia/Jerusalem",
+  coach_priorities: [
+    "עמית גורן",
+    "רועי אופנהיימר",
+    "עומרי פלד",
+    "דניאל טנג'י",
+    "עומר לנדאו",
+    "מתן קדים",
+    "אורפז איבגי",
+  ]
+};

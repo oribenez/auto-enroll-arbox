c0813812921199a22d564c6eb0c4ea8d4d798e04
Ori Ben Ezra <oriben467@gmail.com>
2023-05-05 17:54:29 +0300

    fixed logs

--- data/schedule.js
+++ data/schedule.js
@@ -12,11 +12,11 @@ export const scheduleClasses = [
   {
     "class_name": "WOD",
     "date": "2023-05-10",
-    "start_time": "18:00"
+    "start_time": "18:30"
   },
   {
     "class_name": "WOD",
     "date": "2023-05-11",
-    "start_time": "18:00"
+    "start_time": "18:30"
   }
 ];

326fe632ae81a8f20434d119b4cca6be9d832488
Ori Ben Ezra <oriben467@gmail.com>
2023-05-05 17:50:13 +0300

    small updates

--- data/config.js
+++ data/config.js
@@ -3,7 +3,7 @@ export default {
     email: process.env.ARBOX_USER_EMAIL,
     password: process.env.ARBOX_USER_PASSWORD
   },
-  registerTime: "17:36:00",
+  registerTime: "21:00:00",
   timezone: "Asia/Jerusalem",
   coach_priorities: [
     "עמית גורן",
--- data/schedule.js
+++ data/schedule.js
@@ -1,10 +1,5 @@
 export const scheduleClasses = [
   {
-    "class_name": "סאונה",
-    "date": "2023-05-06",
-    "start_time": "09:00"
-  },
-  {
     "class_name": "WOD",
     "date": "2023-05-07",
     "start_time": "08:30"
